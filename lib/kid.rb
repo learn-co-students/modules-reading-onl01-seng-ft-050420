273ace973d2854e064a228a3c2552900f6dd8a2b
michael thomas <michael.thomas2014@gmail.com>
2020-05-22 12:39:20 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,11 @@
-require_relative './dance_module.rb'
+zrequire_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid 
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+
+end
+
+  
